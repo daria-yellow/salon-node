efcd938bd48e87f738cd7f0d163c5d84f89ffdc9
Daria Pronina <dpronina@openware.center>
2023-08-15 18:18:53 +0200

    add port

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sq7t6lr.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(5001);
+    app.listen(process.env.PORT || 5001);
   })
   .catch((err) => {
     console.log(err);
